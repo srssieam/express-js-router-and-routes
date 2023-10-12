1b1d2243afcc15fda0bebf55bb75f551ee8afd43
srs-sieam <srssieam@gmail.com>
2023-10-12 19:13:37 +0600

    how to send json data to server

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ const userRouter = require("./routes/users.route")
 
 app.use("/api/user",userRouter);
 
-app.use("/", (req, res)=>{
-    res.send("I am a get request at home route")
-    res.end();
+app.use("/register", (req, res)=>{   // send json data to server
+    res.status(200).json({
+        message: "I am register page",
+        statusCode: 200,
+    });
 });
+
+app.use("/login", (req, res)=>{
+    res.send("Hi I am login page")
+})
+
+app.use("/", (req, res)=>{
+    res.send("Hi I am home page")
+});
+
+
+
 app.use((req, res)=>{
     res.send("<h1>404 !!! Not a valid url</h1>")
 })
